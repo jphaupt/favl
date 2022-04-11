82c9488601c6c1dc4de91065cfc73b3397815586
jphaupt <jphaupt@protonmail.com>
2022-04-11 14:40:39 +0200

    Split trees and nodes

--- src/avl_tree.f90
+++ src/avl_tree.f90
@@ -5,37 +5,72 @@
 
 
 module avl_tree
-    use iso_fortran_env, only: dp=>real64
+    use iso_fortran_env, only: dp=>real64, stdout=>output_unit
     implicit none
 
-    type avl_tree_t
+    type avl_node_t
     private
-        type(avl_tree_t), pointer :: left=>null()
-        type(avl_tree_t), pointer :: right=>null()
+        class(avl_node_t), pointer :: left=>null()
+        class(avl_node_t), pointer :: right=>null()
         real(dp), public :: val
         integer :: key
+        integer :: height
+
+    end type avl_node_t
+    type avl_tree_t
+        class(avl_node_t), pointer :: root=>null()
     contains
-        ! TODO not sure if pass(self) should be here...
-        procedure, pass(self) :: insert
-        ! procedure :: insert
+        procedure, pass(self) :: insert=>insert_into_tree
     end type avl_tree_t
+
+    private
+    public :: avl_tree_t
 
 contains
 
-    ! pure 
-    subroutine insert(self, key, val)
-        ! TODO should this be a pointer??
-        class(avl_tree_t), pointer, intent(inout) :: self
+    pure recursive subroutine insert_into_tree(self, key, val)
+        class(avl_tree_t), intent(inout) :: self
         integer, intent(in) :: key
         real(dp), intent(in) :: val
-        if (.not. associated(self)) then
+        call insert_node(self%root, key, val)
+    end subroutine insert_into_tree
+
+    pure recursive subroutine insert_node(node, key, val)
+        ! TODO at the moment, this insert is for a normal BST
+        ! NOT an *AVL* tree!! (...yet)
+        class(avl_node_t), pointer, intent(inout) :: node
+        integer, intent(in) :: key
+        real(dp), intent(in) :: val
+        if (.not. associated(node)) then
+            allocate(node)
             ! this is a null pointer, i.e. empty tree
             ! TODO ? do I need to initialise it?
-            self%val=val
-            self%key=key
-            print*, 'what'
+            node%val=val
+            node%key=key
+            nullify(node%left)
+            nullify(node%right)
+            ! print*, 'what'
+        else if (key < node%val) then ! left insert
+            call insert_node(node%left, key, val)
+        else ! right insert
+            call insert_node(node%right, key, val)
         end if
-        print*, "TODO stub"
-    end subroutine insert
+        ! print*, "TODO stub"
+    end subroutine insert_node
+
+    recursive subroutine print_tree(tree)
+        !! prints tree in infix order
+        !! this is only really for debugging purposes
+        type (avl_node_t), intent(in), pointer :: tree  ! root node
+        if (associated (tree)) then
+            ! write(stdout,*) "left for key ", tree%key
+        call print_tree (tree % left)
+        ! write(stdout,fmt="(1x,i0)", advance="no") tree%key
+        write(stdout,fmt="(1x,i0)") tree%key
+        ! write(stdout,*) "right for key ", tree%key
+        ! if(associated(tree % right)) write(stdout,*) "right"
+        call print_tree (tree % right)
+        end if
+    end subroutine print_tree
 
 end module avl_tree
--- src/main.f90
+++ src/main.f90
@@ -1,8 +1,14 @@
 program main
     use iso_fortran_env, only: dp=>real64
-    use avl_tree, only: avl_tree_t
+    use avl_tree
     implicit none
     !! simply does a sort with an AVL tree
-    type(avl_tree_t) :: test_tree
-    call test_tree%insert(42, 2.0_dp)
+    type(avl_tree_t), pointer :: test_tree
+    ! test_tree=>null()
+    ! !! TODO tree constructor and procedures
+    ! call insert(test_tree, 23, 2.12_dp)
+    ! call insert(test_tree, 25, 1.12_dp)
+    ! call insert(test_tree, 2, 4.12_dp)
+    ! call print_tree(test_tree)
+    print*, 'ran successfully'
 end program main
