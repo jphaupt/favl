d5a90b47d4454c49e1ee58f83c8c71c1f56c8d8c
jphaupt <jphaupt@protonmail.com>
2022-04-12 15:13:03 +0200

    fixed "testing" (I use this term loosely here) file

--- src/main.f90
+++ src/main.f90
@@ -3,12 +3,15 @@ program main
     use avl_tree
     implicit none
     !! simply does a sort with an AVL tree
-    type(avl_tree_t), pointer :: test_tree
-    ! test_tree=>null()
-    ! !! TODO tree constructor and procedures
+    type(avl_tree_t) :: test_tree
+    !! TODO tree constructor and procedures
+    call test_tree%insert(2, 523._dp)
+    call test_tree%insert(4, 2.713_dp)
+    call test_tree%insert(1, 2.12_dp)
+    call test_tree%insert(3, 3.14159_dp)
+    call test_tree%print()
     ! call insert(test_tree, 23, 2.12_dp)
     ! call insert(test_tree, 25, 1.12_dp)
     ! call insert(test_tree, 2, 4.12_dp)
-    ! call print_tree(test_tree)
     print*, 'ran successfully'
 end program main
